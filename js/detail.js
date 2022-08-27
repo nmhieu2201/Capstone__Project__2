77ac726db9e1e567659eb1c4add21205d054c878
nmhieu2201 <minhhieudz.22012000a@gmail.com>
2022-08-28 01:22:32 +0700

    update detail

---
+++ js/detail.js
@@ -0,0 +1,26 @@
+import Shoes from "../models/shoes.js";
+async function getDataAPI() {
+  const shoes = new Shoes();
+  try {
+    var result = await axios({
+      url: "https://shop.cyberlearn.vn/api/Product",
+      method: "GET",
+    });
+    result.data.content.map((shoe) => {
+      shoes.render(shoe);
+    });
+    // renderSinhVien(result.data);
+  } catch (error) {
+    console.log(error, "err");
+  }
+  // promise.then(function (result) {
+  //   console.log(result.data);
+  //   renderSinhVien(result.data);
+  // });
+  // promise.catch(function (err) {
+  //   console.log(err);
+  // });
+}
+window.onload = () => {
+  getDataAPI();
+};
